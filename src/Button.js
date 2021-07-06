5649caca3a5b87aaafaa3b767ff31e51ad6f6cfa
Praveen Kumar <pk1798.kumar@gmail.com>
2021-07-06 12:30:01 +0530

    added buttons

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,9 @@
-const Button = () => {
+const Button = ({variant, title, buttonStyle, hover, shadow, active}) => {
     return ( 
         <div className="button-container">
-            <p>default</p>
-            <button>Default</button>
+            <p>{title}</p>
+            {active === 'disabled' && <button className={`${variant} ${buttonStyle} ${hover} ${shadow}`} disabled>Default</button> }
+            {active !== 'disabled' && <button className={`${variant} ${buttonStyle} ${hover} ${shadow}`}>Default</button>}
         </div>
      );
 }
--- src/Buttons.js
+++ src/Buttons.js
@@ -4,7 +4,26 @@ const Buttons = () => {
     return ( 
         <div className="buttons">
           <h2>Buttons</h2>
-        <Button/>
+            <div className="container">
+              <Button  buttonStyle="font" variant="default" title='<button />'/>
+              <Button  buttonStyle="font" variant="default" hover="hover" title='&:hover &:focus'/>
+            </div>
+            <div className="container">
+             <Button  buttonStyle="font" variant="outline" title='<button variant="outline" />'/>
+             <Button  buttonStyle="font" variant="outline" hover="hover" title='&:hover &:focus'/>
+            </div>
+            <div className="container">
+                <Button buttonStyle="font" variant="text"  title='<button variant="text" />'/>
+                <Button buttonStyle="font" variant="text" hover="hover"  title='&:hover &:focus'/>
+            </div>
+            <div className="container">
+                <Button buttonStyle="font" variant="default" shadow="none" title="<Button disabledShadow/>"/>
+            </div>
+            <div className="container">
+                <Button buttonStyle="font" variant="default" active="disabled" title="<Button disabled/>"/>
+                <Button buttonStyle="font" variant="text" active="disabled" title="<Button disabled/>"/>
+
+            </div>
         </div>
      );
 }
